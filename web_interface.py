9b10bf0a93aaa062a7e9d821fa4c43330e289c23
Fei Xiong <feixiong@andrew.cmu.edu>
2025-07-10 13:43:56 -0400

    update index page, functioning webpage before modifying asset choices

--- web_interface.py
+++ web_interface.py
@@ -574,4 +574,4 @@ if __name__ == '__main__':
     static_dir = Path("static")
     static_dir.mkdir(exist_ok=True)
     
-    app.run(debug=True, host='0.0.0.0', port=5000) 
+    app.run(debug=True, host='127.0.0.1', port=5000) 
