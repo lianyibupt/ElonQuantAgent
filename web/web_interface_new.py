9fde5613847deba910ce8766da5bc252b4779209
elon <lianyibupt@gmail.com>
2026-04-25 20:27:05 +0800

    update

--- web/web_interface_new.py
+++ web/web_interface_new.py
@@ -542,8 +542,7 @@ class WebTradingAnalyzer:
             asset_name = safe_str(asset_name)
             timeframe = safe_str(timeframe)
             
-            # 使用所有数据点
-            df_slice = df.tail(100)  # 获取最近100条数据
+            df_slice = df
             
             # 检查必要的列，注意Datetime可能被设置为索引
             required_price_columns = ["Open", "High", "Low", "Close"]
